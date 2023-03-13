e678f0a0387911fa032c7cd23083b4e28ea4360f
Suraj S Neelagar <surajneelagar@gmail.com>
2023-03-13 18:44:56 +0530

    Changes
    In server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,20 @@
-const http = require('http')
+const http = require('http');
 const mongoose = require('mongoose')
 
-const hostname = 'localhost'
-const port = 3000
+const hostname = 'localhost';
+const port = 2023;
 
-/*  username : ticketingsystem, Password : a6krmaSGoX3O6CL6  */
-// Database Connaction
-mongoose.connect(
-  'mongodb+srv://ticketingsystem:a6krmaSGoX3O6CL6@cluster0.hppktsf.mongodb.net/?retryWrites=true&w=majority'
-  )
-.then(() => {
-  console.log('Connacted to database')
-})
-.catch(() => {
-  console.log('Connaction Failed')
-})
+/* username : ticketingsystem , password : a6krmaSGoX3O6CL6 */
+mongoURL = 'mongodb+srv://ticketingsystem:a6krmaSGoX3O6CL6@cluster0.hppktsf.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect( mongoURL )
+.then(() =>  console.log('Connacted to database')  )
+.catch(() =>  console.log('Connaction failed')   )
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World')
+  res.end('Hello World');
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
